768c547fb7845c91ad695ea19184c33208a01775
slojanko <jankec72@gmail.com>
2023-10-22 20:26:23 +0200

    Push correct version falling sand

--- esp32-falling-sand.ino
+++ esp32-falling-sand.ino
@@ -1,60 +1,231 @@
-
-// Example sketch which shows how to display some patterns
-// on a 64x32 LED matrix
-//
-
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
+#define PANEL_RES_X 64
+#define PANEL_RES_Y 64
+#define PANEL_CHAIN 1
 
-#define PANEL_RES_X 64  // Number of pixels wide of each INDIVIDUAL panel module.
-#define PANEL_RES_Y 64  // Number of pixels tall of each INDIVIDUAL panel module.
-#define PANEL_CHAIN 1   // Total number of panels chained one to another
+#define SAND_COUNT 1000
+#define SAND_SCENE_SCALE 256
+#define SAND_SCENE_X (PANEL_RES_Y * SAND_SCENE_SCALE - 1) // Highest allowed x in sand scene
+#define SAND_SCENE_Y (PANEL_RES_Y * SAND_SCENE_SCALE - 1) // Highest allowed y in sand scene
 
-//MatrixPanel_I2S_DMA dma_display;
-MatrixPanel_I2S_DMA *dma_display = nullptr;
+// Joystick
+#define VRX_PIN 14
+#define VRY_PIN 13
+
+#define MAX_FPS 45
+#define TIME_STEP (1000 / MAX_FPS)
+// #define INCLUDE_vTaskDelay 1
+
+struct Sand {
+	int16_t x, y;
+	int16_t vx, vy;
+	uint16_t panelPixel;
+	uint16_t colour;
+}
+sand[SAND_COUNT];
+
+MatrixPanel_I2S_DMA * dma_display = nullptr;
+uint16_t imagePtr[PANEL_RES_X * PANEL_RES_Y];
 
 void setup() {
-  Serial.begin(9600);
-  delay(500);
-  Serial.println("AAA");
+	Serial.begin(115200);
 
-  // Module configuration
-  HUB75_I2S_CFG mxconfig(
-    PANEL_RES_X,  // module width
-    PANEL_RES_Y,  // module height
-    PANEL_CHAIN   // Chain length
-  );
-  mxconfig.gpio.e = 38;
-  mxconfig.clkphase = false;
+	HUB75_I2S_CFG mxconfig(
+		PANEL_RES_X,
+		PANEL_RES_Y,
+		PANEL_CHAIN
+	);
+	mxconfig.gpio.e = 38;
+	mxconfig.clkphase = false;
 
-  // Display Setup
-  dma_display = new MatrixPanel_I2S_DMA(mxconfig);
-      Serial.println("BBB");
-  dma_display->begin();
-  delay(500);
-  dma_display->setBrightness8(100);  //0-255
-  dma_display->clearScreen();
-  // draw a blue circle
-  dma_display->fillScreen(dma_display->color565(255, 255,255));
-      Serial.println("CCC");
-  delay(500);
-      Serial.println("DDD");
+	dma_display = new MatrixPanel_I2S_DMA(mxconfig);
+	dma_display -> begin();
+	dma_display -> setBrightness8(90); //0-255
+	dma_display -> clearScreen();
+	dma_display -> setRotation(0);
+
+	for (int i = 0; i < SAND_COUNT; i++) {
+		uint16_t panelPixel = 0;
+		do {
+			sand[i].x = random(PANEL_RES_X * SAND_SCENE_SCALE);
+			sand[i].y = random(PANEL_RES_Y * SAND_SCENE_SCALE);
+
+			panelPixel = (sand[i].y / SAND_SCENE_SCALE) * PANEL_RES_X + (sand[i].x / SAND_SCENE_SCALE);
+
+		} while (imagePtr[panelPixel] != 0);
+
+		sand[i].panelPixel = panelPixel;
+		sand[i].vx = 0;
+		sand[i].vy = 0;
+		sand[i].colour = random(256 * 256);
+		imagePtr[panelPixel] = sand[i].colour;
+	}
+
+	// Watchdog is something that runs in background to prevent cpu core being hugged by a single task for too long
+	// You can disable it per core, per task or temporarily put task to sleep
+	// 2nd and 3rd ways are shown in pixelTask function but commented out
+	disableCore0WDT();
+
+	TaskHandle_t xHandle = NULL;
+	xTaskCreatePinnedToCore(pixelTask, "PixelTask1", 5000, 0, (0 | tskIDLE_PRIORITY | INCLUDE_vTaskDelay), & xHandle, 0);
 }
 
-int pos = 0;
-
 void loop() {
-  pos++;
-  if (pos % 2 == 1) {
-    dma_display->fillScreen(dma_display->color565(255, 0,0));
-  } else {
-    dma_display->fillScreen(dma_display->color565(255, 255,255));
-  }
-  // dma_display->drawPixel(pos % 64, pos / 64, dma_display->color444(14,15,15));
-  // pos++;
-  // if (pos >= 64*64) {
-  //   pos = 0;
-  // }
-  // delay(1);
-  delay(100);
+	dma_display -> drawRGBBitmap(0, 0, imagePtr, 64, 64);
+	delay(10);
+}
+
+void pixelTask(void * param) {
+	// Disable watchdog per task
+	// esp_task_wdt_init(5, false);
+
+	uint32_t prevTime = millis();
+	//TickType_t xLastWakeTime = xTaskGetTickCount();
+
+	while (true) {
+		// TickType_t xDelay = (TIME_STEP - (millis() - prevTime)) / portTICK_PERIOD_MS;
+		// if (xDelay > TIME_STEP) {
+		//   xDelay = 1 / portTICK_PERIOD_MS;
+		// }
+		// vTaskDelay(xDelay);	
+		TickType_t xDelay = (TIME_STEP - (millis() - prevTime)) / portTICK_PERIOD_MS;
+		if (xDelay < TIME_STEP) {
+			vTaskDelay(xDelay);
+		}
+
+		// This includes execution of function, so putting 100ms and logic takes 40ms, it will wait for only 60ms
+		// vTaskDelayUntil(&xLastWakeTime, 100 / portTICK_PERIOD_MS);
+		prevTime = millis();
+
+		int16_t ax = ((int16_t) analogRead(VRX_PIN) - 2048) / (8 * 4);
+		int16_t ay = ((int16_t) analogRead(VRY_PIN) - 2048) / (8 * 4);
+		// Remove incorrect reading when joystick not in use - slight randomness in one direction
+		// if (abs(ax) < 8) {
+		//   ax = 0;
+		// }
+		// if (abs(ay) < 8) {
+		//   ay = 0;
+		// }
+
+		// print data to Serial Monitor on Arduino IDE
+		// Serial.print("x = ");
+		// Serial.print(ax);
+		// Serial.print(", y = ");
+		// Serial.println(ay);
+
+		int32_t v2;
+		float v;
+		// Limit max speed
+		for (int i = 0; i < SAND_COUNT; i++) {
+			sand[i].vx += ax + random(-4, 5);
+			sand[i].vy += ay + random(-4, 5);
+
+			v2 = (int32_t) sand[i].vx * sand[i].vx + (int32_t) sand[i].vy * sand[i].vy;
+			if (v2 > 65536) {
+				v = sqrt((float) v2); // Velocity vector magnitude
+				sand[i].vx = (int)(256.0 * (float) sand[i].vx / v);
+				sand[i].vy = (int)(256.0 * (float) sand[i].vy / v);
+			}
+		}
+
+		uint16_t oldidx, newidx, delta;
+		int16_t newx, newy;
+
+		// Calculate next position, if any
+		for (uint16_t i = 0; i < SAND_COUNT; i++) {
+			newx = sand[i].x + sand[i].vx;
+			newy = sand[i].y + sand[i].vy;
+
+			// Clamp inside Scene
+			if (newx > SAND_SCENE_X) {
+				newx = SAND_SCENE_X;
+				sand[i].vx /= -2;
+			} else if (newx < 0) {
+				newx = 0;
+				sand[i].vx /= -2;
+			}
+
+			if (newy > SAND_SCENE_Y) {
+				newy = SAND_SCENE_Y;
+				sand[i].vy /= -2;
+			} else if (newy < 0) {
+				newy = 0;
+				sand[i].vy /= -2;
+			}
+
+			oldidx = sand[i].panelPixel;
+			newidx = (newy / SAND_SCENE_SCALE) * PANEL_RES_X + (newx / SAND_SCENE_SCALE);
+
+			// If new pixel isn't free, try to find alternative
+			if ((oldidx != newidx) && imagePtr[newidx]) {
+				delta = abs(newidx - oldidx);
+
+				// Sand tried to move only horizontally
+				if (delta == 1) {
+					newx = sand[i].x;
+					sand[i].vx /= -2;
+					sand[i].vy *= 3; // Try to topple columns
+					newidx = oldidx;
+				} // Sand tried to move only vertically 
+				else if (delta == PANEL_RES_X) {
+					newy = sand[i].y;
+					sand[i].vx *= 3; // Try to topple columns
+					sand[i].vy /= -2;
+					newidx = oldidx;
+				} // Sand tried to move diagonally, find axis with higher velocity
+				else {
+					// Faster horizontally
+					if ((abs(sand[i].vx) - abs(sand[i].vy)) >= 0) {
+						// Try to forcefully move on x
+						newidx = (sand[i].y / SAND_SCENE_SCALE) * PANEL_RES_X + (newx / SAND_SCENE_SCALE);
+						if (!imagePtr[newidx]) {
+							newy = sand[i].y;
+							sand[i].vy /= -2;
+						} else {
+							// Fallback to y
+							newidx = (newy / SAND_SCENE_SCALE) * PANEL_RES_X + (sand[i].x / SAND_SCENE_SCALE);
+							if (!imagePtr[newidx]) {
+								newx = sand[i].x;
+								sand[i].vx /= -2;
+							} else {
+								newx = sand[i].x;
+								newy = sand[i].y;
+								sand[i].vx /= -2;
+								sand[i].vy /= -2;
+								newidx = oldidx;
+							}
+						}
+					} // Faster verticall 
+					else {
+						// Try to forcefully move on y
+						newidx = (newy / SAND_SCENE_SCALE) * PANEL_RES_X + (sand[i].x / SAND_SCENE_SCALE);
+						if (!imagePtr[newidx]) {
+							newx = sand[i].x;
+							sand[i].vx /= -2;
+						} else {
+							// Fallback to x
+							newidx = (sand[i].y / SAND_SCENE_SCALE) * PANEL_RES_X + (newx / SAND_SCENE_SCALE);
+							if (!imagePtr[newidx]) {
+								newy = sand[i].y;
+								sand[i].vy /= -2;
+							} else {
+								newx = sand[i].x;
+								newy = sand[i].y;
+								sand[i].vx /= -2;
+								sand[i].vy /= -2;
+								newidx = oldidx;
+							}
+						}
+					}
+				}
+			}
+
+			sand[i].x = newx;
+			sand[i].y = newy;
+			imagePtr[oldidx] = 0;
+			imagePtr[newidx] = sand[i].colour;
+			sand[i].panelPixel = newidx;
+		}
+	}
 }
